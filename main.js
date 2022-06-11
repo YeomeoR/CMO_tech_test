1828671bf0f285f4a58f6f19b875ef9078278ee3
Rob Yeomans <robyeomans1@gmail.com>
2022-06-11 14:18:00 +0100

    Add psuedo code in main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-    console.log('document ready');
-})
+  console.log('document ready');
+
+  // if checkbox is checked, html label should read 'This is including VAT'
+  // and background image should be Rishi
+  //      else, html label should read 'This is excluding VAT'
+  //      and styling should be basic
+});
